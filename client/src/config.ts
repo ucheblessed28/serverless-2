07f5e402beee74e7a9a8bba13fa6e7231df24c84
ucheblessed <ucheblessed1@gmail.com>
2022-09-20 09:46:56 +0100

    done

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-0x7a1bom.us.auth0.com',
+  clientId: 'No5Qp5jfvpU4fdaYrzsPqx1pSOdUjAdv',
+  callbackUrl: 'http://localhost:3000/callback'
+};
